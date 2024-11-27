2a8e495fb0622351f260c527cf40aae56016586f
Puneeth K <reachout.puneeth@gmail.com>
2024-11-27 10:09:42 +0530

    succesfully able to conenct the backend the database

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+await connectDB()
+
+app.get('/', (req, res) => {
+    res.send('API WORKING FINE')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
