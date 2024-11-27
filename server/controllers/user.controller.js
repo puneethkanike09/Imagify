4ac711c7976b728df6ec12daa8b474cbc4da8de1
Puneeth K <reachout.puneeth@gmail.com>
2024-11-27 11:42:59 +0530

    Created the Controller function for the user credit balance

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -71,7 +71,17 @@ const loginUser = async (req, res) => {
     }
 }
 
+const userCredits = async (req, res) => {
+    try {
+        const { userId } = req.body;
 
+        const user = await userModel.findById(userId);
+        res.json({ success: true, message: "User credits", credits: user.creditBalance, user: { name: user.name } })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 
 
 export { registerUser, loginUser }
